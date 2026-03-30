322679f42e43c6f64c47a80605a68fd3ba459364
WessamMohamed22 <wm8821730@gmail.com>
2026-03-31 01:56:30 +0200

    Create admin login page and auth theme styles

---
+++ src/constants/auth.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_STORAGE_KEY = "mn_bety_access_token";
+export const USER_STORAGE_KEY = "mn_bety_user";
+export const ACCESS_TOKEN_COOKIE_KEY = "mn_bety_access_token";
+export const USER_ROLES_COOKIE_KEY = "mn_bety_user_roles";
